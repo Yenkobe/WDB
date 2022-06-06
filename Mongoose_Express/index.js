770e33ba0020b014f45032c1114bfb14dec16c6e
Yenduy S <yenduykr@gmail.com>
2022-06-06 10:50:41 +0900

    Updating the product and adding new links

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+//Esto es despues que instalamos en package method override --- chequear la linea 61
+const methodOverride = require('method-override')
+// app.use(methodOverride('_method')) ----- lo movi a donde estan los app.use en la linea25
 
 //export from the product.js
 const Product = require('./models/product');
@@ -20,6 +23,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // esto se necesita para conectar el button, donde se enviara la nueva info.
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+//updated/add new categories
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 // test if is working!! 
 app.get('/products', async (req, res) => {
@@ -29,7 +36,7 @@ app.get('/products', async (req, res) => {
 })
 //creating products
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 //where we want the product to be submitted.
 
@@ -43,12 +50,25 @@ app.post('/products', async (req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    res.render('products/show', { product });
-
+    res.render('products/show', { product })
 })
 // When you send a database query like .find() it needs some time to communicate to the database and get the data back,
 // and when you add the await keyword the code will basically 'pause' and wait to get the data back from the database.
 
+//Updating a Product
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    //find the product byid
+    const product = await Product.findById(id)
+    res.render('products/edit', { product, categories })
+})
+//here we need to instal the method override cuz we are using POST AND HERE WE ARE USING PUT.
+// SO we need to install the package ---- npm i method-override
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${product._id}`);
+})
 app.listen(3000, () => {
     console.log("App is listening on PORT 3000!")
 })
