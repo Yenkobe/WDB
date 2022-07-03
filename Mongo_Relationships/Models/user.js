02675601857768713bdc8c4fa3f68b19c6ac089c
Yenduy S <yenduykr@gmail.com>
2022-07-03 10:33:26 +0900

    adding one to many relationships

--- Mongo_Relationships/Models/user.js
+++ Mongo_Relationships/Models/user.js
@@ -8,3 +8,60 @@ mongoose.connect('mongodb://localhost:27017/relationshipDemo')
         console.log("Oh no ERROR!!")
         console.log(err)
     })
+
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    // Array of String
+    addresses: [
+        {
+            // to turn off id
+            _id: { id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Steven',
+        last: 'Sanchez'
+    })
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+
+}
+
+//Adding difenrent function
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            street: '888 washington St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'
+        }
+    )
+    const res = await user.save()
+    console.log(res)
+}
+
+addAddress('62c0ed42475f0e1427409922');
+// makeUser();
