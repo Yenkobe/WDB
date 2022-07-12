e7393ebb5579352c3ef1cb12a7612b8fb42c6f0e
Yenduy S <yenduykr@gmail.com>
2022-07-12 17:30:14 +0900

    AuthDemo: Registering

--- Auth/AuthDemo/index.js
+++ Auth/AuthDemo/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+//conect mongo
+mongoose.connect('mongodb://localhost:27017/authDemo')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!")
+    })
+    .catch(() => {
+        console.log("Oh NO MONGO CONNECTION ERROR!!")
+        console.log(err)
+    })
+app.use(express.urlencoded({ extended: true }));
 
 // setup ejs
 app.set('view engine', 'ejs');
@@ -8,6 +22,24 @@ app.set('views', 'views');
 
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE!')
+})
+
+//using bcrypt
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    //storaging that into the db
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
+
 })
 
 app.get('/secret', (req, res) => {
