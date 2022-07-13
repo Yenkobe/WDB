c184815c2b95a709d20872beb815804d73d6159f
Yenduy S <yenduykr@gmail.com>
2022-07-13 19:44:44 +0900

    AuthDemo: Logout

--- Auth/AuthDemo/index.js
+++ Auth/AuthDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 
 //conect mongo
@@ -14,36 +15,69 @@ mongoose.connect('mongodb://localhost:27017/authDemo')
         console.log("Oh NO MONGO CONNECTION ERROR!!")
         console.log(err)
     })
+
+
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }));
+
 
 // setup ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE!')
 })
 
-//using bcrypt
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+//Register using bcrypt
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
-    //storaging that into the db
+    //storing that into the db
     const user = new User({
         username,
         password: hash
     })
     await user.save();
-    res.redirect('/');
+    req.session.user_id = user._id;
+    res.redirect('/secret');
 
 })
 
+//Login
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    //find user by username
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (validPassword) {
+        // means if you successfully log in we are storing your user_id in the session
+        req.session.user_id = user._id;
+        res.redirect('/secret');
+    } else {
+        res.send('/login');
+    }
+})
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    res.redirect('/login');
+})
+
+
 app.get('/secret', (req, res) => {
-    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    res.render('secret')
 })
 
 
